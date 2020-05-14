ece155c230798351c5356064e4a38028b866dfb3
Morgan Collado <morgan.collado@gmail.com>
2020-05-14 01:28:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  new_hash = {}
-  name_hash.each do |key, value|
+  name_hash.collect do |key, value|
     if value <= 1
-    new_hash[key] = key
   end 
  end 
 end
